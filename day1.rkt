728bf31ccb803be81db1d8c02c6c22a5aae9d512
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2022-12-01 19:42:48 +0100

    Find max and solve puzzle 1

--- day1.rkt
+++ day1.rkt
@@ -6,7 +6,14 @@
 
 (define (split-list lst pred?) (foldl (lambda (next st) (if (pred? next) (cons '() st) (replace-head st (lambda (h) (cons next h))))) (list '()) lst))
 
+(define (sum-list lst) (foldl + 0 lst))
+
+(define (max-list lst) (foldl max 0 lst))
+
 (let* (
     [lines (file->lines "day1/input.txt" #:mode 'text #:line-mode 'linefeed)]
-    [split (split-list lines string-empty?)])
-    (print split))
+    [split (split-list lines string-empty?)]
+    [numbers (map (lambda (g) (map string->number g)) split)]
+    [summed (map sum-list numbers)]
+    [max-cals (max-list summed)])
+    (print max-cals))
