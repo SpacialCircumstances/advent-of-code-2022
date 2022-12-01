c9961b385dc9f88d929e9cdd03286ae4b8e1f35a
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2022-12-01 20:06:24 +0100

    Solve puzzle 2

--- day1.rkt
+++ day1.rkt
@@ -10,10 +10,19 @@
 
 (define (max-list lst) (foldl max 0 lst))
 
+(define (zero-list n) (build-list n (lambda (_) 0)))
+
+(define (max-n-list lst n) (foldl 
+    (lambda (next st) 
+        (sort (cons (max next (car st)) (cdr st)) <)) (zero-list n) lst))
+
 (let* (
     [lines (file->lines "day1/input.txt" #:mode 'text #:line-mode 'linefeed)]
     [split (split-list lines string-empty?)]
     [numbers (map (lambda (g) (map string->number g)) split)]
     [summed (map sum-list numbers)]
-    [max-cals (max-list summed)])
-    (print max-cals))
+    [max-cals (max-list summed)]
+    [max-3-cals (max-n-list summed 3)]
+    [total-max-3-cals (sum-list max-3-cals)])
+    (displayln max-cals)
+    (displayln total-max-3-cals))
