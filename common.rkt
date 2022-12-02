d3bcd552ad1d065a848c02d8de3919e0479c1ab9
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2022-12-02 12:29:18 +0100

    Refactor commonly usable functions out

--- common.rkt
+++ common.rkt
@@ -16,13 +16,4 @@
     (lambda (next st) 
         (sort (cons (max next (car st)) (cdr st)) <)) (zero-list n) lst))
 
-(let* (
-    [lines (file->lines "day1/input.txt" #:mode 'text #:line-mode 'linefeed)]
-    [split (split-list lines string-empty?)]
-    [numbers (map (lambda (g) (map string->number g)) split)]
-    [summed (map sum-list numbers)]
-    [max-cals (max-list summed)]
-    [max-3-cals (max-n-list summed 3)]
-    [total-max-3-cals (sum-list max-3-cals)])
-    (displayln max-cals)
-    (displayln total-max-3-cals))
+(provide (all-defined-out))
