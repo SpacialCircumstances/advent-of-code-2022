bb0d2029cf4ca0dc2c8586039ca40b81d3e3af5b
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2022-12-03 15:36:57 +0100

    Solve day 3 puzzle 1

--- day3/day3.rkt
+++ day3/day3.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+(require "../common.rkt")
+
 (define (parse-compartments line) (let* (
     [tr (string-trim line)]
     [len (string-length tr)]
@@ -12,8 +14,14 @@
 
 (define (find-common-items compartments) (apply set-intersect compartments))
 
+(define (item-priority item) (if (char-upper-case? item) (- (char->integer item) 38) (- (char->integer item) 96)))
+
+(define (calc-items-priority items) (sum-list (map item-priority (set->list items))))
+
 (let* (
     [lines (file->lines "day3/input.txt")]
     [compartments (map parse-compartments lines)]
-    [common-items (map find-common-items compartments)])
-    (displayln common-items))
+    [common-items (map find-common-items compartments)]
+    [priorities (map calc-items-priority common-items)]
+    [total-priority (sum-list priorities)])
+    (displayln total-priority))
